aedbc04d157dbae537251885bda3cc4a7edba348
Lucas Burch <lucasburch@hotmail.com>
2022-07-28 21:00:00 -0300

    Validação de Senha e armazenamento Cadastro
    Dados são armazenados em localStorage agora. Também deu pra utilizar a validação do email como referência para criar uma função que validasse a senha com RegEx. Acabei utilizando o span de erro somente para senha e os demais retirei e ajustei a estilização. Amanhã o Login deve ficar redondo também :D

--- AluraChallengeFront/js/validacoes.js
+++ AluraChallengeFront/js/validacoes.js
@@ -1,5 +1,6 @@
 // Selecionando campos do formulário (uma ideia para refatorar depois, com tempo, seria aproveitar os spans de campo inválido que resolvi abandonar depois de não conseguir aplicar a confirmação de senha com os objetos do curso de validação da Alura)
 
+let botaoCadastro = document.querySelector('#botao-cadastro');
 let btn = document.querySelector('#verSenha');
 let btnConfirm = document.querySelector('#verConfirmeSenha');
 
@@ -7,19 +8,20 @@ let nome = document.querySelector('#nome');
 let labelNome = document.querySelector('#label-nome');
 let validNome = false;
 
-// Faltou a do e-mail; precisaria de uma expressão regular ainda para fazer seu evento.
 let email = document.querySelector('#email');
 let labelEmail = document.querySelector('#label-email');
 let validEmail = false;
 
 let senha = document.querySelector('#senha');
-let labelSenha = document.querySelector('#label-senha');
+let erroSenha = document.querySelector('#erro-senha');
 let validSenha = false;
 
 let confirmSenha = document.querySelector('#confirm-senha');
 let labelConfirmSenha = document.querySelector('#label-confirm-senha');
 let validConfirmSenha = false;
 
+let msgErro = document.querySelector('#msg-erro');
+let msgSucesso = document.querySelector('#msg-sucesso');
 
 // Eventos para validação:
 // nome:
@@ -41,25 +43,23 @@ email.addEventListener('keyup', () => {
     labelEmail.setAttribute('style', 'color: red');
     labelEmail.innerHTML = 'Email *Modelo: nome@texto.com';
     validEmail = false;
-    console.log(validEmail)
   } else {
     labelEmail.setAttribute('style', 'color: green');
     labelEmail.innerHTML = 'Email';
     validEmail = true;
-    console.log(validEmail)
   };
 });
 
 
 // senha:
 senha.addEventListener('keyup', () => {
-  if (senha.value.length <= 5) {
-    labelSenha.setAttribute('style', 'color: red');
-    labelSenha.innerHTML = 'Senha *Insira de 6 a 12 caracteres';
+  if (!validarSenha(senha.value)) {
+    erroSenha.setAttribute('style', 'display: block');
+    erroSenha.innerHTML = 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula e minúscula, um número e não deve conter símbolos';
     validSenha = false;
   } else {
-    labelSenha.setAttribute('style', 'color: green');
-    labelSenha.innerHTML = 'Senha';
+    erroSenha.setAttribute('style', 'display: none');
+    erroSenha.innerHTML = '';
     validSenha = true;
   };
 });
@@ -77,6 +77,12 @@ confirmSenha.addEventListener('keyup', () => {
   };
 });
 
+// Função para validar a senha:
+function validarSenha(senha) {
+  var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[ !@#$%^&*_=+-]).{6,12}$/;
+  return regex.test(senha);
+}
+
 // Função para validar o email:
 function validarEmail(email) {
   var regex = /\S+@\S+\.\S+/;
@@ -86,10 +92,43 @@ function validarEmail(email) {
 
 // Função para cadastrar os dados:
 function cadastrar() { // ou add array existente, ou cria um novo/vazio
-  if (validNome && validSenha && validConfirmSenha) {
+  if (validNome && validEmail && validSenha && validConfirmSenha) {
     let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
-  }
-}
+
+    listaUser.push(
+      {
+        nomeCad: nome.value,
+        emailCad: email.value,
+        senhaCad: senha.value,
+      } // confirmSenha não precisa!!
+    ) // pois a senha já foi registrada
+
+    localStorage.setItem('listaUser', JSON.stringify(listaUser));
+
+    msgSucesso.setAttribute('style', 'display: block');
+    msgSucesso.innerHTML = '<p><strong>Cadastrando novo usuário...</strong></p>'
+    msgErro.setAttribute('style', 'display: none');
+    msgErro.innerHTML = '';
+
+
+    setTimeout(() => {
+      window.location.href = '../Login/index.html';
+    }, 3000) // 3s, 3000ms para mudar para login
+
+
+  } else {
+    msgErro.setAttribute('style', 'display: block');
+    msgErro.innerHTML = '<p><strong>Preencha os campos corretamente para cadastrar.</strong></p>';
+    msgSucesso.setAttribute('style', 'display: none');
+    msgSucesso.innerHTML = '';
+  };
+};
+
+// Executando a função com o botão de Cadastrar:
+botaoCadastro.addEventListener('click', (evento) => {
+  evento.preventDefault();
+  cadastrar();
+});
 
 
 // Eventos do botão de olhinho. (ideia para refatorar este seria descobrir como posicionar o olho sempre à direita do input, pois ele sempre sai do lugar com position absolute)
